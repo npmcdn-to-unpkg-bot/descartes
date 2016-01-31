00a7c6966c56f8ffc6aac7bca6a61ab763a41e9a
Brian Ji <jibrianh@gmail.com>
2016-01-30 20:08:48 -0800

    iso flow

--- app/public/js/results.js
+++ app/public/js/results.js
@@ -1,9 +1,12 @@
 'use strict';
 var grid = document.getElementsByClassName('iso-grid');
+var iso;
 
-var iso = new Isotope(grid[0], {
-	'itemSelector': '.grid-item',
-	'layoutMode': 'masonry'
+var loaded = new imagesLoaded(grid[0], function() {
+	iso = new Isotope(grid[0], {
+		'itemSelector': '.grid-item',
+		'layoutMode': 'masonry'
+	});
+
+	grid[0].classList.remove('hidden');
 });
-
-console.log('Results script loaded');
--- app/resources/views/results/index.blade.php
+++ app/resources/views/results/index.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 @section('content')
     <h2>Results</h2>
-	<div class="iso-grid">
+	<div class="iso-grid hidden">
 		@foreach($emoticons as $emoticon)
 			@if ($emoticon->type == "image" || $emoticon->type == "gif")
 				<div class="grid-item">
@@ -16,6 +16,7 @@
 	</div>
 @endsection
 @section('scripts')
+	<script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/2.2.2/isotope.pkgd.min.js"></script>
 	<script src="../js/results.js"></script>
 @endsection
